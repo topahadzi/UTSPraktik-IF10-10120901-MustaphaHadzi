cbcdd005af8342a632d1a7c3a4d119f8bada8532
topahadzi <topahadzi@gmail.com>
2023-05-30 20:08:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if10_10120901_mustaphahadzi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if10_10120901_mustaphahadzi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
